c123411da277f6611984163e75d1289fca9bb426
Michal Swiatczak <michal21x@gmail.com>
2023-09-30 14:30:54 +0200

    Add capacitors, map and coordinates

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,114 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
+  <div>
+  <capacitor-google-map
+  ref="mapRef"
+  style="display: inline-block; width: 100vw; height: 86vh"
+  >
+  </capacitor-google-map>
+  </div>
+  Aktualna lokalizacja: {{ currentLocation.lat }}, {{ currentLocation.lng }}
+  </template>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
-</template>
-
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-</script>
-
-<style scoped>
-#container {
-  text-align: center;
+  <style>
+  .gm-bundled-control.gm-bundled-control-on-bottom,
+  .gm-bundled-control.gm-bundled-control-on-top {
+    display: none !important;}
+  .gm-fullscreen-control {
+    display: none !important;}
+  </style>
   
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
   
-  color: #8c8c8c;
+  <script setup lang="ts">
+  import { onMounted, nextTick, ref, onUnmounted } from "vue";
+  import { GoogleMap } from "@capacitor/google-maps";
+  import { Geolocation, GeolocationPosition } from '@capacitor/geolocation';
+  import { watch } from "vue";
   
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
+  const currentLocation = ref({ lat: 0, lng: 0 });
+  
+  // props
+  const props = defineProps<{
+  markerData: { coordinate: any; title: string; snippet: string }[];
+  }>();
+  // eventy
+  const emits = defineEmits<{
+  (event: "onMarkerClicked", info: any): void;
+  (event: "onMapClicked"): void;
+  }>();
+  
+  const mapRef = ref<HTMLElement>();
+  const markerIds = ref<string[] | undefined>();
+  let newMap: GoogleMap;
+  
+  onMounted(async () => {
+  console.log("mounted ", mapRef.value);
+  await nextTick();
+  await createMap();
+  });
+  
+  // kasowanie znacznikow
+  onUnmounted(() => {
+  console.log("onunmounted");
+  newMap.removeMarkers(markerIds?.value as string[]);
+  });
+  
+  const addSomeMarkers = async (newMap: GoogleMap) => {
+  markerIds?.value && newMap.removeMarkers(markerIds?.value as string[]);
+  
+  // narysuj kazdy punkt na mapie
+  let markers = props.markerData.map(({ coordinate, title, snippet }) => {
+  return {
+  coordinate,
+  title,
+  snippet,
+  };
+  });
+  
+  markerIds.value = await newMap.addMarkers(markers);
+  };
+  
+  async function createMap() {
+  if (!mapRef.value) return;
+  
+  // pobiernaie aktualnej pozycji
+  try {
+    const position = await Geolocation.getCurrentPosition();
+    const { latitude, longitude } = position.coords;
+  
+    // renderowanie mapy i aktualizowanie centrowania
+    newMap = await GoogleMap.create({
+      id: "my-cool-map",
+      element: mapRef.value,
+      apiKey: "AIzaSyDWYUDxr7VCsb-ZBSuXhatZt74hegrReKk",
+      config: {
+        center: {
+          lat: latitude,
+          lng: longitude,
+        },
+        zoom: 12,
+      },
+    });
+    
+  
+    // dodanie markerow
+    addSomeMarkers(newMap);
+  
+    // event nasluchiwania
+    newMap.setOnMarkerClickListener((event) => {
+      emits("onMarkerClicked", event);
+    });
+  
+    newMap.setOnMapClickListener(() => {
+      emits("onMapClicked");
+    });
+  
+    // aktualizacja pozycji
+    currentLocation.value = { lat: latitude, lng: longitude };
+    console.log("Aktualna lokalizacja:", currentLocation.value);
+  } catch (error) {
+    console.error("Error getting current location:", error);
+  }
+  
+  }
+  </script>
+  
