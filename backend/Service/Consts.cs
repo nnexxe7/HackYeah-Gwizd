24f01de99b1cde8b722fb30d8d6af3af03cd5b56
msamociuk <msamociuk@o2.pl>
2023-10-01 09:51:04 +0200

    .

--- backend/Service/Consts.cs
+++ backend/Service/Consts.cs
@@ -3,7 +3,7 @@
 public static class Consts
 {
 	public const double SingularityPrecision = 0.25f;
-	public const double ExpiryTimeInHours = 5;
+	public const double ExpiryTimeInHours = 10;
 	public const double ExpiryBump = 0.5f;
 
 	public const int PointsForToot = 10;
